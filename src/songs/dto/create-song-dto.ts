724d52fcc9f50e88853ce1613405ec87aeb395c8
QuaziSamiha <quazi.samiha4@gmail.com>
2025-10-12 07:17:22 +0600

    doc added

--- src/songs/dto/create-song-dto.ts
+++ src/songs/dto/create-song-dto.ts
@@ -1,26 +1,35 @@
-import {
-  IsArray,
-  IsDateString,
-  IsMilitaryTime,
-  IsNotEmpty,
-  IsString,
-} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateSongDto {
   @IsString()
   @IsNotEmpty()
   readonly title: string;
-
-  @IsNotEmpty()
-  @IsArray()
-  @IsString({ each: true })
-  readonly artists: string[];
-
-  @IsDateString()
-  @IsNotEmpty()
-  readonly releasedDate: string; // must be string, not Date
-
-  @IsMilitaryTime()
-  @IsNotEmpty()
-  readonly duration: string;
 }
+
+//? ==================== CHAPTER 1-5 ============
+// import {
+//   IsArray,
+//   IsDateString,
+//   IsMilitaryTime,
+//   IsNotEmpty,
+//   IsString,
+// } from 'class-validator';
+
+// export class CreateSongDto {
+//   @IsString()
+//   @IsNotEmpty()
+//   readonly title: string;
+
+//   @IsNotEmpty()
+//   @IsArray()
+//   @IsString({ each: true })
+//   readonly artists: string[];
+
+//   @IsDateString()
+//   @IsNotEmpty()
+//   readonly releasedDate: string; // must be string, not Date
+
+//   @IsMilitaryTime()
+//   @IsNotEmpty()
+//   readonly duration: string;
+// }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,37 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class SongsService {
-  // local db
-  // local array
+  constructor(private prisma: PrismaService) {}
 
-  private readonly songs: string[] = [];
-
-  create(song) {
-    // Save the song in the database
-    this.songs.push(song);
-    return this.songs;
+  create(createSongDto: Prisma.SongCreateInput) {
+    return this.prisma.song.create({
+      data: createSongDto,
+    });
   }
 
-  // ? ======== VERSION 2 =========
   findAll() {
-    // fetch the songs from the db
-    throw new Error('Error in Db while fetching record');
-    return this.songs;
+    return this.prisma.song.findMany();
   }
-  //   findAll() {
-  //     // Simulating DB error
-  //     throw new HttpException(
-  //       'Error while fetching songs from DB',
-  //       HttpStatus.INTERNAL_SERVER_ERROR,
-  //     );
 
-  //     return this.songs;
-  //   }
-
-  // ? ======== VERSION 1 =============
-  //   findAll() {
-  //     // fetch the songs from the db
-  //     return this.songs;
-  //   }
+  findOne(id: number) {
+    return this.prisma.song.findUnique({
+      where: { id },
+    });
+  }
 }
+
+// ? =============== CHAPTER 1-5 ==============
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class SongsService {
+//   // local db
+//   // local array
+
+//   private readonly songs: string[] = [];
+
+//   create(song) {
+//     // Save the song in the database
+//     this.songs.push(song);
+//     return this.songs;
+//   }
+
+//   // ? ======== VERSION 2 =========
+//   findAll() {
+//     // fetch the songs from the db
+//     throw new Error('Error in Db while fetching record');
+//     return this.songs;
+//   }
+//   //   findAll() {
+//   //     // Simulating DB error
+//   //     throw new HttpException(
+//   //       'Error while fetching songs from DB',
+//   //       HttpStatus.INTERNAL_SERVER_ERROR,
+//   //     );
+
+//   //     return this.songs;
+//   //   }
+
+//   // ? ======== VERSION 1 =============
+//   //   findAll() {
+//   //     // fetch the songs from the db
+//   //     return this.songs;
+//   //   }
+// }
